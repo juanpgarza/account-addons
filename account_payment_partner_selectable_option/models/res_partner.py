da5ae24f7b323d96f1597460c8a9f82ce8af3e7e
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-24 10:48:19 -0300

    [15.0][ADD] account_payment_partner_selectable_option

---
+++ account_payment_partner_selectable_option/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    payment_selectable = fields.Boolean(string="Seleccionable en Recibos / Ordenes de Pago", default=True)
