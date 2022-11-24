30672232c4e0215aa550afc68cb29265ed970629
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-24 10:48:19 -0300

    [15.0][ADD] account_payment_partner_selectable_option

---
+++ account_payment_partner_selectable_option/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Partner Selectable Option",
+    "version": "15.0.1.0.1",
+    "category": "Accounting",
+    "website": "https://github.com/juanpgarza/account-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": 
+        [
+            "account",
+            "account_payment_group",
+        ],
+    "data": 
+        [
+            "views/res_partner_view.xml"
+        ],
+    "installable": True,
+}
