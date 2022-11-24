78935448a6093ceacab6d7044a7ad23019e2d78d
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-24 12:01:56 -0300

    [15.0][ADD] account_move_partner_selectable_option

---
+++ account_move_partner_selectable_option/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Partner Selectable Option",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/juanpgarza/account-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": 
+        [
+            "account",
+        ],
+    "data": 
+        [
+            "views/res_partner_view.xml"
+        ],
+    "installable": True,
+}
