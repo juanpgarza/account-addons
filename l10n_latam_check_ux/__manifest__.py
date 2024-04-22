78ae866ddbd3884a2017531c94eb56176794d22f
Juan Pablo Garza <juanp@juanpgarza.com>
2024-04-22 18:16:31 -0300

    [15.0][IMP] l10n_latam_check_ux: Control de Nro de cheques repetidos en Ordenes de Pago

--- l10n_latam_check_ux/__manifest__.py
+++ l10n_latam_check_ux/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "l10n_latam_check_ux",
     "summary": "",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/account-addons",
     "author": "juanpgarza",
