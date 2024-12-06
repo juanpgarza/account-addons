b37e9d5bd9086f97641498205d0992f3fce61a52
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-06 23:37:01 +0000

    [MIG] account_move_partner_selectable_option: Migration to 17.0

--- account_move_partner_selectable_option/__manifest__.py
+++ account_move_partner_selectable_option/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Payment Partner Selectable Option",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/account-addons",
     "author": "juanpgarza",
@@ -15,5 +15,5 @@
         [
             "views/res_partner_view.xml"
         ],
-    "installable": False,
+    "installable": True,
 }
