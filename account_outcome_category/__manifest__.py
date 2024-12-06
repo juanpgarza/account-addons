066a3a4163a8d9efabfb5391b99033910b2c8a1d
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-06 23:22:57 +0000

    [MIG] account_outcome_category: Migration to 17.0

--- account_outcome_category/__manifest__.py
+++ account_outcome_category/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Clasificación de facturas de compras",
-    "version": "15.0.1.1.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/account-addons",
     "author": "juanpgarza",
@@ -17,5 +17,5 @@
             "views/account_outcome_category_views.xml",
             'security/ir.model.access.csv',
         ],
-    "installable": False,
+    "installable": True,
 }
