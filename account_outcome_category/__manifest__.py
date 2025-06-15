38448fb60d45f4e93e8725014168827c7422908a
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-06-15 12:57:06 +0000

    [MIG] account_outcome_category: Migration to 18.0

--- account_outcome_category/__manifest__.py
+++ account_outcome_category/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Clasificación de facturas de compras",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/account-addons",
     "author": "juanpgarza",
